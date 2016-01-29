7e4e527d241e3a42bb91cfb2f024e5f71732ab9f
Christopher Henkel <c.henkel@online.de>
2016-01-29 14:07:49 +0100

    2 maps

--- global.R
+++ global.R
@@ -22,20 +22,43 @@ goevb$id = seq.int(nrow(goevb))
 source('customFunctions.R')
 
 # Aggregates all trips with the same origin latitude and longitude and count them.
-locationTrips <-
+locationTripsOrigin <-
   as.data.frame(table(goevb$origin_lon, goevb$origin_lat))
 # Gives the prior created columns names to address them.
-names(locationTrips) <- c('origin_lon', 'origin_lat', 'freq')
-# TODO: Add destination latitude and longitude to locationTrips !!
-# Add origin longitude to locationTrips.
-locationTrips$origin_lon <-
-  as.numeric(as.character(locationTrips$origin_lon))
-# Add origin latitude to locationTrips.
-locationTrips$origin_lat <-
-  as.numeric(as.character(locationTrips$origin_lat))
+names(locationTripsOrigin) <- c('origin_lon', 'origin_lat', 'freq')
+# TODO: Add destination latitude and longitude to locationTripsOrigin !!
+# Add origin longitude to locationTripsOrigin.
+locationTripsOrigin$origin_lon <-
+  as.numeric(as.character(locationTripsOrigin$origin_lon))
+# Add origin latitude to locationTripsOrigin.
+locationTripsOrigin$origin_lat <-
+  as.numeric(as.character(locationTripsOrigin$origin_lat))
 # Filter out all Trips with freq = 0.
-locationTrips <- subset(locationTrips, freq > 0)
+locationTripsOrigin <- subset(locationTripsOrigin, freq > 0)
 
-locationTrips$freq_normalized <- NormalizeFreq(locationTrips$freq)
-locationTrips$freq_a <- GenerateAlpha(locationTrips$freq_normalized)
-locationTrips$freq_c <- GenerateColor(locationTrips$freq_normalized)
+# TODO: Extract as function
+# Aggregates all trips with the same origin latitude and longitude and count them.
+locationTripsDestination <-
+  as.data.frame(table(goevb$destination_lon, goevb$destination_lat))
+# Gives the prior created columns names to address them.
+names(locationTripsDestination) <- c('destination_lon', 'destination_lat', 'freq')
+# TODO: Add destination latitude and longitude to locationTripsDestination !!
+# Add origin longitude to locationTripsDestination.
+locationTripsDestination$destination_lon <-
+  as.numeric(as.character(locationTripsDestination$destination_lon))
+# Add origin latitude to locationTripsDestination.
+locationTripsDestination$destination_lat <-
+  as.numeric(as.character(locationTripsDestination$destination_lat))
+# Filter out all Trips with freq = 0.
+locationTripsDestination <- subset(locationTripsDestination, freq > 0)
+
+
+locationTripsOrigin$freq_normalized <- NormalizeFreq(locationTripsOrigin$freq)
+locationTripsOrigin$freq_a <- GenerateAlpha(locationTripsOrigin$freq_normalized)
+locationTripsOrigin$freq_c <- GenerateColor(locationTripsOrigin$freq_normalized)
+locationTripsOrigin$freq_r <- GenerateRadius(locationTripsOrigin$freq_normalized)
+
+locationTripsDestination$freq_normalized <- NormalizeFreq(locationTripsDestination$freq)
+locationTripsDestination$freq_a <- GenerateAlpha(locationTripsDestination$freq_normalized)
+locationTripsDestination$freq_c <- GenerateColor(locationTripsDestination$freq_normalized)
+locationTripsDestination$freq_r <- GenerateRadius(locationTripsDestination$freq_normalized)
--- server.R
+++ server.R
@@ -6,41 +6,47 @@ function(input, output, session) {
       setView(lng = 9.925, lat = 51.54, zoom = 12)
     map
   })
-  
-  
-  tripsInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(locationTrips[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-    
-    subset(
-      locationTrips,
-      origin_lat >= latRng[1] & origin_lat <= latRng[2] &
-        origin_lon >= lngRng[1] & origin_lon <= lngRng[2]
-    )
+  output$map2 <- renderLeaflet({
+    map2 <- leaflet() %>%
+      addTiles(urlTemplate = "//{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png") %>%
+      setView(lng = 9.925, lat = 51.54, zoom = 12)
+    map2
   })
   
-  # Precalculate the breaks we'll need for the  histograms
-  # TODO: Display on the right side of the dashboard
-  freqBreaks <-
-    hist(plot = FALSE, locationTrips$freq, breaks = 30)$breaks
-  
-  output$histFreq <- renderPlot({
-    if (nrow(tripsInBounds()) == 0)
-      return(NULL)
-    
-    hist(
-      tripsInBounds()$freq,
-      breaks = freqBreaks,
-      main = "Trip count (visible trips)",
-      xlab = "Trip",
-      xlim = range(locationTrips$freq),
-      col = '#00DD00',
-      border = 'white'
-    )
-  })
+  # TODO: Use for dynamic histogram
+#   tripsInBounds <- reactive({
+#     if (is.null(input$map_bounds))
+#       return(locationTrips[FALSE,])
+#     bounds <- input$map_bounds
+#     latRng <- range(bounds$north, bounds$south)
+#     lngRng <- range(bounds$east, bounds$west)
+#     
+#     subset(
+#       locationTrips,
+#       origin_lat >= latRng[1] & origin_lat <= latRng[2] &
+#         origin_lon >= lngRng[1] & origin_lon <= lngRng[2]
+#     )
+#   })
+#   
+#   # Precalculate the breaks we'll need for the  histograms
+#   # TODO: Display on the right side of the dashboard
+#   freqBreaks <-
+#     hist(plot = FALSE, locationTrips$freq, breaks = 30)$breaks
+#   
+#   output$histFreq <- renderPlot({
+#     if (nrow(tripsInBounds()) == 0)
+#       return(NULL)
+#     
+#     hist(
+#       tripsInBounds()$freq,
+#       breaks = freqBreaks,
+#       main = "Trip count (visible trips)",
+#       xlab = "Trip",
+#       xlim = range(locationTrips$freq),
+#       col = '#00DD00',
+#       border = 'white'
+#     )
+#   })
   
   # This observer is responsible for maintaining the circles and legend,
   # according to the variables the user has chosen to map to color and size.
@@ -48,18 +54,31 @@ function(input, output, session) {
   observe({
     dayBy <- input$day
     hourBy <- input$hour
-    
-    leafletProxy("map", data = locationTrips) %>%
+    # TODO: adapt circle size when zooming in.
+    leafletProxy("map", data = locationTripsOrigin) %>%
       clearShapes() %>%
       addCircles(
         ~ origin_lon,
         ~ origin_lat,
-        radius = 175,
+        radius = ~ freq_r,
         layerId = ~ origin_lon,
         stroke = FALSE,
         color = ~ freq_c,
-        fillOpacity = ~ freq_a
+        fillOpacity = ~ 0.5
       )
+    # Extract as function
+    leafletProxy("map2", data = locationTripsDestination) %>%
+      clearShapes() %>%
+      addCircles(
+        ~ destination_lon,
+        ~ destination_lat,
+        radius = ~ freq_r,
+        layerId = ~ destination_lon,
+        stroke = FALSE,
+        color = ~ freq_c,
+        fillOpacity = ~ 0.5
+      )
+    
   })
   
   # Show a popup at the given location
